ea650bb7b9e9f421fad9c312e36f64a9dc2550ca
Shalazary <dfcz.petro@gmail.com>
2021-02-26 23:51:20 +0300

    Make ListNode class nested

--- MyContainer/src/main/java/MyContainer/List.java
+++ MyContainer/src/main/java/MyContainer/List.java
@@ -1,49 +1,80 @@
 package MyContainer;
 
-public class List{
+public class List {
+
+    class ListNode {
+        private int value;
+        private ListNode next;
+
+        public ListNode getNext() {
+            return next;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setNext(ListNode next) {
+            this.next = next;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public ListNode(ListNode next, int value) {
+            this.value = value;
+            this.next = next;
+        }
+
+        public ListNode(int value) {
+            this(null, value);
+        }
+    }
+
     private int size;
     private ListNode head;
     private ListNode tail;
 
-    public List(){
+    public List() {
         size = 0;
         head = null;
         tail = null;
     }
 
-    public int lenght(){
+    public int lenght() {
         return size;
     }
 
-    private ListNode getNodeAt(int index){
-        //if(index < 0 || index >= size)
+    private ListNode getNodeAt(int index) {
+        // if(index < 0 || index >= size)
         ListNode current = head;
-        for(int i = 0; i < index; i++)
+        for (int i = 0; i < index; i++)
             current = current.getNext();
         return current;
     }
 
-    public int getValueAt(int index){
-        //if(index < 0 || index >= size)
+    public int getValueAt(int index) {
+        // if(index < 0 || index >= size)
         return getNodeAt(index).getValue();
     }
 
-    public void insertAt(int index, int value){
-        //if(index < 0 || index >= size)
-        if(size == 0 || index == size - 1){
+    public void insertAt(int index, int value) {
+        // if(index < 0 || index >= size)
+        if (size == 0 || index == size - 1) {
             insertBack(value);
-        } else if(index == 0) {
+        } else if (index == 0) {
             head = new ListNode(head, value);
         } else {
-        ListNode current = getNodeAt(index - 1);
-        current.setNext(new ListNode(current.getNext(), value));
-        size++;
+            ListNode current = getNodeAt(index - 1);
+            current.setNext(new ListNode(current.getNext(), value));
+            size++;
         }
     }
 
-    public void insertBack(int value){
+    public void insertBack(int value) {
         ListNode node = new ListNode(value);
-        if(head == null){
+        if (head == null) {
             head = node;
             tail = node;
         } else {
@@ -53,9 +84,9 @@ public class List{
         size++;
     }
 
-    public void insertFront(int value){
+    public void insertFront(int value) {
         ListNode node = new ListNode(head, value);
-        if(head == null){
+        if (head == null) {
             head = node;
             tail = node;
         } else {
@@ -64,14 +95,14 @@ public class List{
         size++;
     }
 
-    public void removeAt(int index){
-        //if(index < 0 || index >= size)
-        if(index == 0){
+    public void removeAt(int index) {
+        // if(index < 0 || index >= size)
+        if (index == 0) {
             head = head.getNext();
         } else {
             ListNode prev = getNodeAt(index - 1);
             prev.setNext(prev.getNext().getNext());
-            if(index == size - 1)
+            if (index == size - 1)
                 tail = prev;
         }
         size--;
@@ -80,8 +111,8 @@ public class List{
     @Override
     public String toString() {
         String s = new String();
-        if(size > 0){
-            for(int i = 0; i < size - 1; i++){
+        if (size > 0) {
+            for (int i = 0; i < size - 1; i++) {
                 s += getValueAt(i) + " -> ";
             }
             s += tail.getValue();
