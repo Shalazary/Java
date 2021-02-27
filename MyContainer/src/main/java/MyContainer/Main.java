f0681e03dbbde152b39641c10b5e6f107a4b5228
Shalazary <dfcz.petro@gmail.com>
2021-02-27 16:14:28 +0300

    Remove code from Main

--- MyContainer/src/main/java/MyContainer/Main.java
+++ MyContainer/src/main/java/MyContainer/Main.java
@@ -2,8 +2,5 @@ package MyContainer;
 
 public class Main{
     public static void main(String[] args) {
-        List excepted = new List(new int[] {1, 2, 3, 4, 5});
-
-        System.out.println("Some");
     }
 }
