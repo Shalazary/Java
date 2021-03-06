232c6ba4d4b207c57d8edf4228b587ce53a70ba3
Shalazary <dfcz.petro@gmail.com>
2021-03-07 00:21:29 +0300

    Add Expression Calculator

---
+++ ExpressionCalculator/src/main/java/Shalazary/MainClass.java
@@ -0,0 +1,7 @@
+package Shalazary;
+
+public class MainClass {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ ExpressionCalculator/src/test/java/Shalazary/ExpressionCalculatorTest.java
@@ -0,0 +1,9 @@
+package Shalazary;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class ExpressionCalculatorTest extends TestCase {
+
+}
