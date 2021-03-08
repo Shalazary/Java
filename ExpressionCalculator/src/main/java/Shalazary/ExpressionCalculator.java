a497d9951616e4a2eb599d49ad0ee7b35c6c65b2
Shalazary <dfcz.petro@gmail.com>
2021-03-08 21:10:01 +0300

    Сreated a template for ExpressionCalculator

--- ExpressionCalculator/src/main/java/Shalazary/ExpressionCalculator.java
+++ ExpressionCalculator/src/main/java/Shalazary/ExpressionCalculator.java
@@ -1,6 +1,22 @@
 package Shalazary;
 
-public class ExpressionCalculator 
-{
-    
+import java.util.HashMap;
+import java.util.List;
+
+public class ExpressionCalculator {
+    private String ExpressionString;
+    private String[] PostfixExpression;
+    private HashMap<String, Double> VariablesTable;
+
+    public ExpressionCalculator(String expression) {
+
+    }
+
+    public void setExpression(String expression) {
+
+    }
+
+    public Double compute(HashMap<String, Double> variablesTable) {
+        return 0.0;
+    }
 }
